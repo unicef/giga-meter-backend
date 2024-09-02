8c3e1aca83bee07bb57469f1861027a75cfa962a
Aditya Acharya <aditya.acharya@nagarro.com>
2024-09-02 19:13:33 +0530

    add data fix tests

---
+++ src/data-fix/data-fix.controller.spec.ts
@@ -0,0 +1,29 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { DataFixController } from './data-fix.controller';
+import { HttpModule } from '@nestjs/axios';
+import { AuthGuard } from '../auth/auth.guard';
+
+describe('DataFixController', () => {
+  let controller: DataFixController;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [DataFixController],
+      providers: [AuthGuard],
+      imports: [HttpModule],
+    }).compile();
+
+    controller = app.get<DataFixController>(DataFixController);
+  });
+
+  it('controller should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('DataFix', () => {
+    it('should return empty result set', async () => {
+      const response = await controller.dataFix();
+      expect(response.data).toStrictEqual([]);
+    });
+  });
+});
