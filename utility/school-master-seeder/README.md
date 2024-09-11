c0c4e64d56df66de38452ce0a79b25aa6349b958
arushsinghh <arush.singh@nagarro.com>
2024-09-11 13:31:01 +0530

    School master seeder utility (#29)

---
+++ utility/school-master-seeder/README.md
@@ -0,0 +1,70 @@
+# Python Utility Script for Syncing API Data with PostgreSQL
+
+This Python utility script fetches data from an API, checks if it exists in a PostgreSQL table, and inserts new records if necessary. It is designed for Python 3.8+ and uses SQLAlchemy for database interactions.
+
+## Table of Contents
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Configuration](#configuration)
+- [Running the Script](#running-the-script)
+
+## Prerequisites
+- **Python 3.8+**: Ensure Python 3.8+ is installed.
+- **PostgreSQL**: Ensure you have PostgreSQL installed and running.
+- **PIP**: Ensure `pip` is installed for managing Python packages.
+
+## Installation
+
+### 1. How to Install Python 3.8
+You can download and install Python 3.8 from the official Python website [here](https://www.python.org/downloads/release/python-380/).
+
+#### On Ubuntu:
+```bash
+sudo apt update
+sudo apt install python3.8
+```
+
+### 2. How to Install PIP
+```bash
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+```
+
+#### On Ubuntu:
+```bash
+sudo apt update
+sudo apt-get install python3-pip
+```
+
+### 3. How to Install Virtual Environment
+#### On Windows,
+```bash
+pip install virtualenv
+use `venv\Scripts\activate`
+```
+
+#### On Ubuntu:
+```bash
+python3 -m venv venv
+source venv/bin/activate  
+```
+
+### 4. How to Install requirements.txt
+```bash
+pip install -r requirements.txt
+```
+
+## Configuration
+#### Edit the config.py file:
+Before running the script, configure your settings in the config.py file. 
+This file contains variables such as your API token, database connection URL,
+lookup fields, and column mappings.
+
+## Running-the-script
+### 5. How to run
+```bash
+python3 file_name.py
+```
+
+
+
---
+++ utility/school-master-seeder/sync_data.py
@@ -0,0 +1,75 @@
+import requests
+import pandas as pd
+from sqlalchemy import create_engine, exc, text
+from config import *
+import json
+
+def fetch_data_from_api():
+    headers = {'Authorization': f'Bearer {SOURCE_API_TOKEN}'}
+    response = requests.get(SOURCE_API, headers=headers)
+    response.raise_for_status()
+    data = response.json()
+    return data
+
+def get_next_id(engine):
+    max_id_query = text(f"SELECT COALESCE(MAX(id), 0) AS max_id FROM {DESTINATION_TABLE}")
+    try:
+        with engine.connect() as connection:
+            result = connection.execute(max_id_query).fetchone()
+            next_id = result[0] + 1
+            return next_id
+    except exc.SQLAlchemyError as e:
+        print(f"Error fetching max id: {e}")
+
+def check_data_exists(data, engine):
+    try:
+        existing_data = pd.read_sql(
+            f"SELECT {', '.join(DESTINATION_TABLE_LOOKUP_FIELDS)} FROM {DESTINATION_TABLE}",
+            engine
+        )
+        for id_source_field, id_dest_field in zip(SOURCE_LOOKUP_FIELDS, DESTINATION_TABLE_LOOKUP_FIELDS):
+            if data[id_source_field].isnull().any():
+                raise ValueError(f"Source field '{id_source_field}' contains null values.")
+            if existing_data[id_dest_field].isnull().any():
+                raise ValueError(f"Destination field '{id_dest_field}' contains null values.")
+
+            new_data = data[~data[id_source_field].isin(existing_data[id_dest_field])]
+            if not new_data.empty:
+                break
+        return new_data
+    except Exception as ex:
+        print(f"Error in check_data_exists: {ex}")
+
+def insert_data(data, engine):
+    data = data.rename(columns=column_mapping)
+    data['country_code'] = DEFAULT_COUNTRY_CODE
+    if 'feature_flags' in data.columns:
+        data['feature_flags'] = data['feature_flags'].apply(json.dumps)
+    data.to_sql(DESTINATION_TABLE, engine, index=False, if_exists="append")
+
+def connect_to_db():
+    try:
+        engine = create_engine(DATABASE_URL)
+        print("Connected to database successfully!")
+        return engine
+    except exc.SQLAlchemyError as e:
+        print(f"Error connecting to database: {e}")
+
+def main():
+    api_data = fetch_data_from_api()
+    data = api_data['data']
+    df = pd.DataFrame(data)
+    school_data = df[list(column_mapping.keys())]
+    engine = connect_to_db()
+    if not engine:
+        return
+
+    new_data = check_data_exists(school_data, engine)
+    if new_data is not None and not new_data.empty:
+        print("New data found, inserting...")
+        insert_data(new_data, engine)
+    else:
+        print("No new data to insert.")
+
+if __name__ == "__main__":
+    main()
