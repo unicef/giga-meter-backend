6747ec65462bc8cd5ee328e8e23ed7a1fe49785e
Aditya Acharya <aditya.acharya@nagarro.com>
2024-09-25 20:29:10 +0530

    update filter

--- src/common/common.filter.ts
+++ src/common/common.filter.ts
@@ -19,24 +19,10 @@ export class AllExceptionFilter implements ExceptionFilter {
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR;
 
-    if (
-      exception instanceof HttpException &&
-      (httpStatus === HttpStatus.BAD_REQUEST ||
-        httpStatus === HttpStatus.UNAUTHORIZED ||
-        httpStatus === HttpStatus.FORBIDDEN ||
-        httpStatus === HttpStatus.NOT_FOUND ||
-        httpStatus === HttpStatus.METHOD_NOT_ALLOWED)
-    ) {
-      response.status(httpStatus).json({
-        statusCode: httpStatus,
-        message: exception.message,
-      });
-    } else {
-      Sentry.captureException(exception);
-      response.status(httpStatus).json({
-        statusCode: httpStatus,
-        message: 'Internal server error',
-      });
-    }
+    Sentry.captureException(exception);
+    response.status(httpStatus).json({
+      statusCode: httpStatus,
+      message: exception.message,
+    });
   }
 }
