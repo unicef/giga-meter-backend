2cc7b643b21e7e2680cc8fa76678a054bfb8521a
Aditya Acharya <aditya.acharya@nagarro.com>
2024-10-29 15:40:54 +0530

    metrics logic update

--- src/metrics/metrics.service.ts
+++ src/metrics/metrics.service.ts
@@ -8,9 +8,14 @@ export class MetricsService {
 
   async get(): Promise<MetricsDto> {
     const countries = await this.prisma.dailycheckapp_country.count();
-    const schools = await this.prisma.dailycheckapp_school.count();
+    const schools = await this.prisma.dailycheckapp_school.groupBy({
+      by: 'giga_id_school',
+      _count: {
+        giga_id_school: true,
+      },
+    }); // ideal solution should be using distinct: 'giga_id_school', but it was not working so it is an alternate solution
     const measurements = await this.prisma.measurements.count();
 
-    return { countries, schools, measurements };
+    return { countries, schools: schools?.length, measurements };
   }
 }
