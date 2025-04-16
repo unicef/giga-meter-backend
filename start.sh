c2cc545f6eddb5db95e8bc8e511581b220b6b3db
Shubham Goel <130149461+shubhamgoel02@users.noreply.github.com>
2025-04-16 18:07:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+# export environment variables to make them available in ssh session
+for var in $(compgen -e); do
+    echo "export $var=${!var}" >> /etc/profile
+done
+
+eval $(printenv | awk -F= '{print "export " "\""$1"\"""=""\""$2"\"" }' >> /etc/profile)
+
 echo "Starting SSH ..."
 service ssh start
 
