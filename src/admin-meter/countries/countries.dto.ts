28578a2239ff34909ae196402bef654cd75a246b
shantanunagarro-tech <shantanu.sharma01@nagarro.com>
2026-01-05 19:26:01 +0530

    adding country management apis

---
+++ src/admin-meter/countries/countries.dto.ts
@@ -0,0 +1,48 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsInt, IsString, IsOptional, IsEnum } from 'class-validator';
+import { Type } from 'class-transformer';
+import { SpeedTestProtocol } from '@prisma/client';
+import { CommonResponseDto } from 'src/utility/dto';
+
+export class CountriesListingDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  limit = 20;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  page = 1;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsString()
+  search: string;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsInt()
+  @Type(() => Number)
+  country_id = 0;
+}
+
+export class CountryFieldToggleDto {
+  @ApiPropertyOptional()
+  @IsOptional()
+  @IsEnum(SpeedTestProtocol)
+  speed_test_protocol?: SpeedTestProtocol;
+
+  @ApiPropertyOptional()
+  @IsOptional()
+  @Type(() => Boolean)
+  is_active?: boolean;
+}
+
+export class CountryToggleStatusDtoResponse extends CommonResponseDto {
+  @ApiProperty({ type: Object })
+  @Type(() => Object)
+  data: any;
+}
