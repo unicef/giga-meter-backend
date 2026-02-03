f9fb619dfad1ffab8f8d77308226e2c76f6a140b
Nitesh <nitesh02@nagarro.com>
2026-02-03 11:43:17 +0530

    removed unused service

--- src/admin-meter/cms/cms.module.ts
+++ src/admin-meter/cms/cms.module.ts
@@ -4,7 +4,6 @@ import { MediaController } from './controllers/media.controller';
 import { ContentService } from './services/content.service';
 import { MediaService } from './services/media.service';
 import { StorageService } from './services/storage.service';
-import { SchemaValidationService } from './services/schema-validation.service';
 import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
@@ -14,7 +13,6 @@ import { PrismaModule } from '../../prisma/prisma.module';
     ContentService,
     MediaService,
     StorageService,
-    SchemaValidationService,
   ],
   exports: [ContentService, MediaService],
 })
